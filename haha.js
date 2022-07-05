4673cf0425a98e887c6c0fa49e146dee33b11342
muugii123 <bmunkhtuvshin22@gmail.com>
2022-07-05 16:46:07 +0800

    added work1

---
+++ haha.js
@@ -0,0 +1,5 @@
+// document.getElementsByClassName('text').style.color = "#ff0000";
+// const element = document.getElementsByClassName('text').style.color;
+// console.log(element)
+
+const element = document.getElementById('haha').style.color = 'red';
